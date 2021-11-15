57dac79f4e02f27e8eb7359c873fc72d02ceb888
Narendrakumar <kumawatnrndr291@gmail.com>
2021-11-15 16:05:39 +0530

    heroku configs added

--- url_project/settings.py
+++ url_project/settings.py
@@ -133,3 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# // to deploy on heroku
+django_heroku.settings(locals())
